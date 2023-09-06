7dcd739189af294e49ce24a0b827134da8a81b43
Kachi <justiceChinedu156@gmail.com>
2023-09-06 14:20:35 +0100

    Add existing project files to Git

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <h1 className="font-bold text-red-500">Hello TailwindCSS with raeact</h1>
+    </>
+  )
+}
+
+export default App
